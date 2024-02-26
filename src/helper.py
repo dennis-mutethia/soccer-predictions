41717fc737e07c38b8042211b9fc66384990d529
Dennis Muga <dennis.mutethia1@accesscfa.com>
2024-02-26 23:31:22 +0300

    add timeout

--- src/helper.py
+++ src/helper.py
@@ -1,19 +1,31 @@
 import requests
 
 class Helper:
+    """
+    This class provides helper methods for fetching data.
+    """
 
-    def fetch_data(self, url):
+    def fetch_data(self, url, timeout=10):
+        """
+        Fetch data from the given URL.
+
+        Args:
+            url (str): The URL to fetch data from.
+            timeout (int, optional): The timeout for the HTTP request.
+
+        Returns:
+            dict or None: The JSON data if successful, otherwise None.
+        """
         headers = {
             'User-Agent': 'PostmanRuntime/7.36.3',
         }
 
-        response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers, timeout=timeout)
         if response.status_code == 200:
             json_data = response.json()
             if json_data:
                 return json_data
-            else:
-                print("Invalid JSON data format")
+            print("Invalid JSON data format")
         else:
             print(f"Failed to fetch data. Status Code: {response.status_code}")
 
