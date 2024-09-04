66b13b2118f25127c033b800252518b10aa0923b
Dennis Muga <dennis.mutethia1@accesscfa.com>
2024-09-04 21:05:21 +0300

    subscribe

--- at.py
+++ at.py
@@ -9,18 +9,23 @@ class AT():
         self.short_code = '22384'
         self.keyword = 'TIP'
         
+        self.subscription = Subscription()
         self.postgres_crud = PostgresCRUD()
         self.premium_sms = PremiumSMS()
         self.broadcast = Broadcast()
     
+    def send_sub_push(self, phone_number):
+        #self.subscription.create_subscription_sync(self.short_code, self.keyword, phone_number)
+        self.premium_sms.subscribe(self.short_code, self.keyword, phone_number) 
+        
     def fetch_and_save_subs(self):    
-        phone_numbers = Subscription().fetch_subscriptions_sync(self.short_code, self.keyword)
+        phone_numbers = self.subscription.fetch_subscriptions_sync(self.short_code, self.keyword)
         for phone_number in phone_numbers:
             self.postgres_crud.add_or_remove_subscriber(phone_number, status=1)
     
     def send_premium_sms_to_subscribed(self):  
         message = self.broadcast.upcoming_sms()
-        active_subscribers = self.postgres_crud.fetch_subscribers(1)
+        active_subscribers = self.postgres_crud.fetch_subscribers(0)
         recipients = ''
         for subscriber in active_subscribers:
             recipients = recipients + f'+{subscriber[0]},'
@@ -39,6 +44,7 @@ class AT():
                                                 
              
 if __name__ == '__main__':
+    AT().send_sub_push("+254105565532")
     #AT().fetch_and_save_subs()
-    AT().send_premium_sms_to_subscribed()
+    #AT().send_premium_sms_to_subscribed()
         
--- utils/africastalking/premium_sms.py
+++ utils/africastalking/premium_sms.py
@@ -9,7 +9,9 @@ class PremiumSMS:
 		# Set your app credentials
         load_dotenv()
 
-        self.url = os.getenv('AT_URL')
+        self.token_url = os.getenv('AT_TOKEN_URL')
+        self.subscription_url = os.getenv('AT_SUBSCRIPTION_URL')
+        self.content_url = os.getenv('AT_CONTENT_URL')
         self.username = os.getenv('AT_USERNAME')
         self.api_key = os.getenv('AT_API_KEY')
         self.headers = {
@@ -17,6 +19,28 @@ class PremiumSMS:
             'Content-Type': 'application/x-www-form-urlencoded',
             'apiKey': self.api_key
         }
+    
+    def subscribe(self, shortcode, keyword, phone_number): 
+        phone_number = urllib.parse.quote_plus(phone_number)
+        
+        payload = f'phoneNumber={phone_number}'
+        print(payload)
+        
+        try:
+            response = requests.request("POST", self.token_url, headers=self.headers, data=payload)
+            checkoutToken = response.json()["token"]
+            print(checkoutToken)
+            
+            payload = f'username={self.username}&shortCode={shortcode}&keyword={keyword}&phoneNumber={phone_number}&checkoutToken={checkoutToken}'
+            print(payload)
+            response = requests.request("POST", self.subscription_url, headers=self.headers, data=payload)
+            print(response.text)
+
+            return response.json()
+            
+        except Exception as err:
+            print(f"An error occurred: {err}")
+            return None
     
     def send(self, shortcode, keyword, recipients, message): 
         recipients = urllib.parse.quote_plus(recipients)
