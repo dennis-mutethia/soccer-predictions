3c65a0e4dbdb169719133e750e24abd4ecf8a3d8
Dennis Muga <dennis.mutethia1@accesscfa.com>
2025-02-16 11:07:50 +0300

    date format

--- auto_bet.py
+++ auto_bet.py
@@ -39,7 +39,7 @@ class AutoBet():
                     prediction = predictions.get(key)
                     #key = key.replace('BTTS','YES')
                     prediction = prediction if isinstance(prediction, int) else double(prediction.replace('%', ''))
-                    if (key in ['1', 'X', '2', 'OVER 2.5'] and prediction>=75) or (key=='OVER 1.5' and prediction>=85):  
+                    if (key == 'OVER 2.5' and prediction>=75) or (key=='OVER 1.5' and prediction>=85):  
                         url = f'https://api.betika.com/v1/uo/match?parent_match_id={parent_match_id}'
                         match_details = self.betika.fetch_data(url)
                         data = match_details.get('data')
@@ -50,7 +50,7 @@ class AutoBet():
                                     odds = d.get('odds')
                                     for odd in odds:
                                         odd_value = odd.get('odd_value')
-                                        if key == odd.get('display') and double(odd_value)<=1.6 and parent_match_id not in added_parent_match_ids:
+                                        if key == odd.get('display') and double(odd_value)>=1.2 and double(odd_value)<=1.6 and parent_match_id not in added_parent_match_ids:
                                             bet_pick = odd.get('odd_key')
                                             special_bet_value = odd.get('special_bet_value')
                                             outcome_id = odd.get('outcome_id')
